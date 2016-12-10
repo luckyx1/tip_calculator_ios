b25e1d046385b7b5cce486b03ebc4c431f6ff185
ROBERT HERNANDEZ <robert.hernandez@coupa.com>
2016-12-10 13:39:28 -0800

    Created Blank IOS app

---
+++ tip_calculator/tip_calculator/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  tip_calculator
+//
+//  Created by Rob Hernandez on 12/10/16.
+//  Copyright © 2016 Robert Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
+
